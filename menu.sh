a07633cda70fd5332cc0f11a7ac443276f4e48ff
Salomon CHUNG <salomon.chung@u-pe.fr>
2012-11-14 21:31:35 +0100

    C'est presque bon. Mais je vais quand même lancer les simulations.

--- menu.sh
+++ menu.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 echo "Monte-Carlo Canonique : Menu"
-echo "	go! : compilation et exécution d'un programme."
-echo "	go!!! : compilation et exécution de plusieurs programmes."
-echo "	b : bunching pour obtenir l'erreur."
+#echo "	n : nettoyage (binaires, données)."
+echo "	go! : compilation et exécution de programme(s)."
+#echo "	b : bunching pour obtenir l'erreur."
 echo "	p : trace les graphes."
 echo "	l : lois physiques." # pompeux
 echo "	q : quitter le menu."
@@ -16,12 +16,9 @@ case ${choix} in
 	go!)
 		echo "	go! : compilation et exécution."
 		Scripts/make+exec.sh ;;
-	go!!!)
-		echo "	go!!! : compilation et exécution de plusieurs programmes."
-		Scripts/make+exec_multi.sh;;
-	b)
-		echo "	b : bunching pour obtenir l'erreur."
-		Scripts/bunching.sh ;;
+	#b)
+		#echo "	b : bunching pour obtenir l'erreur."
+		#Scripts/bunching.sh ;;
 
 	p)
 		echo "	p : trace les graphes."
