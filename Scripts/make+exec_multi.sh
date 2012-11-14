d93042a19a8c6b31e43ad906ed41bfa6b99bcc43
Salomon CHUNG <salomon.chung@u-pe.fr>
2012-11-14 20:50:16 +0100

    Je bloque au niveau des données. Pas facile : comment utiliser le bunching lorsque les simulations sont finies.

--- Scripts/make+exec_multi.sh
+++ Scripts/make+exec_multi.sh
@@ -7,8 +7,10 @@ exec1="xMC_Canonique"
 exec2="xBunching"
 
 condIni="cube"
-execFold="Bin"
+binFold_i="Bin"
+outFold_i="Bin"
 editeur="gedit"
+exec1NewCore="xMC_Cano"
 
 # ---------------------------------------------------------
 
@@ -50,7 +52,7 @@ cd ${binFold}
 		iSimu=$(expr ${iSimu} \+ 1)
 		echo "Création de Bin"${iSimu}"."
 	
-		mkdir ${execFold}${iSimu}
+		mkdir ${binFold_i}${iSimu}
 
 	done
 
@@ -84,9 +86,9 @@ cd ${srcFold}
 			exit
 		fi
 		
-		exec1New=${exec1}"_"${param}${iSimu}
-		mv -i ${exec1} ../${binFold}/${execFold}${iSimu}/${exec1New}
-		mv -i ${exec2} ../${binFold}/${execFold}${iSimu}
+		exec1New=${exec1NewCore}"_"${param}${iSimu}
+		mv -i ${exec1} ../${binFold}/${binFold_i}${iSimu}/${exec1New}
+		mv -i ${exec2} ../${binFold}/${binFold_i}${iSimu}
 
 	done
 
@@ -102,10 +104,10 @@ cd ${binFold}
 
 		iSimu=$(expr ${iSimu} \+ 1)
 		
-		cd ${execFold}${iSimu}
+		cd ${binFold_i}${iSimu}
 			
 			echo
-			exec1New=${exec1}"_"${param}${iSimu}
+			exec1New=${exec1NewCore}"_"${param}${iSimu}
 			echo "Exécution de "${exec1New}
 			
 			dateIni[${iSimu}]=$(date)
@@ -123,4 +125,33 @@ cd ${binFold}
 
 cd ..
 
+# Données
+cd ${outFold}
 
+	iSimu=0
+	until test ${iSimu} -eq ${nSimus}
+	do
+		iSimu=$(expr ${iSimu} \+ 1)
+	
+		mkdir ${outFold_i}${iSimu}
+		cd ${outFold_i}${iSimu}
+
+		mv ../../${binFold}/${binFold_i}${iSimu}/*.out .
+
+		echo " Date : " >> rapport.out
+		echo "     Début : " ${dateIni[${iSimu}]} >> rapport.out
+
+		# Bunching
+		cp ../../${binFold}/${binFold_i}${iSimu}/xBunching .
+		./xBunching 14
+		rm xBunching
+
+		# Rapport	
+		cp ../../Scripts/avgRms.sh .
+		./avgRms.sh
+		rm avgRms.sh
+		cat rapport.out
+	
+	done
+	
+cd ..
