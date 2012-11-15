181d51786c1495630e414913712183eda3c6da97
Salomon CHUNG <salomon.chung@u-pe.fr>
2012-11-15 10:07:03 +0100

    Les simulations se sont bien passées. Par contre, le traitement des données ne s'est pas fait. Je vais récrire le script de bunching en multi.

--- Scripts/bunching.sh
+++ Scripts/bunching.sh
@@ -3,21 +3,55 @@
 binFold="Binaries"
 outFold="Out"
 
+binFold_i="Bin"
+
+nSimus=$(cat Temp/nSimus.in)
+
 echo "Entrez le nombre d'itérations :"
 read nBunching
-
-ls ${outFold}/*.out
-if test $? -ne 0
-then
-	echo "Pas de données."
-	exit
-fi
 
 cd ${outFold}
 
 	# Bunching
-	cp ../${binFold}/xBunching .
-	./xBunching ${nBunching}
-	rm xBunching
+	#cp ../${binFold}/xBunching .
+	#./xBunching ${nBunching}
+	#rm xBunching
+	
+cd ..
+
+# Données
+cd ${outFold}
+
+	iSimu=0
+	until test ${iSimu} -eq ${nSimus}
+	do
+		
+		iSimu=$(expr ${iSimu} \+ 1)
+
+		cd ${outFold_i}${iSimu}
+		
+			pwd
+		
+			ls *.out
+			if test $? -ne 0
+			then
+				echo "Pas de données."
+				exit
+			fi			
+
+			# Bunching
+			#cp ../../${binFold}/${binFold_i}${iSimu}/xBunching .
+			#./xBunching ${nSimus}
+			#rm xBunching
+
+			# Rapport	
+			#cp ../../Scripts/avgRms.sh .
+			#./avgRms.sh
+			#rm avgRms.sh
+			#cat rapport.out
+		
+		cd ..
+	
+	done
 	
 cd ..
