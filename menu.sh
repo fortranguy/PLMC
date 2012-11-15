4ce279ef430ef96745f702adb0b5f24dca96e066
Salomon CHUNG <salomon.chung@u-pe.fr>
2012-11-15 12:45:46 +0100

    Un accident est si vite arrivé.

--- menu.sh
+++ menu.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 echo "Monte-Carlo Canonique : Menu"
-#echo "	n : nettoyage (binaires, données)."
+echo "	n : nettoyage (binaires et données)."
 echo "	go! : compilation et exécution de programme(s)."
-#echo "	b : bunching pour obtenir l'erreur."
-echo "	p : trace les graphes."
+echo "	b : bunching pour obtenir l'erreur."
+#echo "	p : trace les graphes."
 echo "	l : lois physiques." # pompeux
 echo "	q : quitter le menu."
 
@@ -12,17 +12,21 @@ read choix
 
 
 case ${choix} in
+
+	n)
+		echo "	n : nettoyage (binaires, données)."
+		Scripts/clean.sh ;;
 	
 	go!)
 		echo "	go! : compilation et exécution de programme(s)."
 		Scripts/make+exec.sh ;;
-	#b)
-		#echo "	b : bunching pour obtenir l'erreur."
-		#Scripts/bunching.sh ;;
+	b)
+		echo "	b : bunching pour obtenir l'erreur."
+		Scripts/bunching.sh ;;
 
-	p)
-		echo "	p : trace les graphes."
-		Scripts/plots.sh ;;
+	#p)
+		#echo "	p : trace les graphes."
+		#Scripts/plots.sh ;;
 		
 	l)
 		echo "	l : lois physiques."
