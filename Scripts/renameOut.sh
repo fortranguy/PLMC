40db615577e679968311958e7a2a6c23c70c3611
Salomon CHUNG <salomon.chung@u-pe.fr>
2012-11-15 19:38:00 +0100

    Ce script sert à renommer les dossiers pour continuer une variation.

---
+++ Scripts/renameOut.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+outBin_i="Out"
+
+echo "Décalage : Out1 -> Outi, i entier"
+
+cd Out
+
+	listOut=$(ls |sort -n)
+	nOut=$(ls | wc -l)
+	
+	echo ${listOut}
+	
+	# Entrée du décalage
+	
+	status="non"
+		
+	until [ ${status} = "oui" ]
+	do
+	
+		echo "Entrez le nombre de décalage : "
+		read nDecal
+	
+		iSimuDecal=$(expr 1 \+ ${nDecal})
+		echo "Acceptez-vous le changement Out1 -> Out"${iSimuDecal}" ? (oui/non)"
+		read status
+		
+		if test ${iSimuDecal} -le ${nOut}
+		then
+			echo "Le nombre est trop petit."
+			ls
+			status="non"
+		fi
+			
+		
+	done
+	
+	# Vérification
+
+	nSimusDecal=$(expr ${iSimuDecal} \+ ${nOut})
+	
+	until test ${iSimuDecal} -eq ${nSimusDecal}
+	do
+	
+		ls ${outBin_i}${iSimuDecal} 2&> /dev/null
+		if test $? -eq 0
+		then
+			echo "Le dossier n°"${iSimuDecal}" existe."
+			exit
+		fi
+		
+		iSimuDecal=$(expr ${iSimuDecal} \+ 1)
+		
+	done
+	
+	# Changement de nom
+	
+	iSimuDecal=$(expr 1 \+ ${nDecal})
+	
+	for out_i in ${listOut}
+	do		
+		mv ${out_i} ${outBin_i}${iSimuDecal}
+		
+		echo ${out_i}"->Out"${iSimuDecal}
+		iSimuDecal=$(expr ${iSimuDecal} \+ 1)
+	
+	done
+
+cd ..
