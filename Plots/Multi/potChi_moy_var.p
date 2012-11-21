0377ee23617c0ebe3079f4c506905b7a4a4233e3
Salomon CHUNG <salomon.chung@u-pe.fr>
2012-11-21 10:49:33 +0100

    Pour plu

--- Plots/Multi/potChi_moy_var.p
+++ Plots/Multi/potChi_moy_var.p
@@ -4,6 +4,11 @@ file=dataFold."/".dataFold_i."/".outFold."/".out
 outHs="potChi_hs_var.out"
 fileHs=dataFold."/".dataFold_i."/".outFold."/".outHs
 
+id = 2
+ex = 3
+
+y=ex
+
 set title "Statistique : Potentiel chimque : moyenne"
 set xlabel "Ncol"
 set xrange[Ndeb:Nfin]
@@ -12,5 +17,5 @@ set ylabel "Moyenne"
 
 mu(eta) = (eta * (8 - 9*eta + 4*eta**2))/(1-eta)**3
 
-plot file u 1:2:(2.*$3/$1) w l, "" u 1:2:(2.*$3/$1) with errorbars, \
-    fileHs u 1:(mu($2) + $3) w l t "HS"
+plot file u 1:y w l, "" u 1:y:(2.*$4/$1) with errorbars\
+    , fileHs u 1:(mu($2) + $3) w l t "HS"
--- Scripts/Plots/potChi_stats_var.sh
+++ Scripts/Plots/potChi_stats_var.sh
@@ -13,15 +13,19 @@ do
 	cd ${outFold_i}${iSimu}
 	
 		Ncol=$(grep "Ncol1" rapport.out | cut -d = -f 2)
-		potChiMoy=$(grep "potChi.moy" rapport.out | cut -d = -f 2)
+		potChiIdMoy=$(grep "Potentiel chimique idéal " rapport.out | \
+		    cut -d = -f 2)
+		potChiExMoy=$(grep "Potentiel chimique (excès) moyen" rapport.out | \
+		    cut -d = -f 2)
 		potChiRms=$(grep "potChiEx.rms" rapport.out | cut -d = -f 2)
 		
 		potChiErreur=$(awk 'NR==1 {print $3/$2}' bunching_activInv.out)
 		#Delta potChi = Delta activInv / activInv
 			# premier : suffisant ?
 		
-		echo ${Ncol}"	"${potChiMoy}"	"${potChiErreur} >> ../potChi_moy_var.out
-		echo ${Ncol}"	"${potChiRms} >> ../potChi_rms_var.out
+		echo "${Ncol}    ${potChiIdMoy} ${potChiExMoy}   ${potChiErreur}" >> \
+		    ../potChi_moy_var.out
+		echo "${Ncol}    ${potChiRms}" >> ../potChi_rms_var.out
 		
 		echo "Rapport n°"${iSimu}" lu."
 		
