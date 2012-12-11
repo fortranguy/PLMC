cc2f9aaee8167f1ca249f4e76bf5133069e66390
Salomon CHUNG <salomon.chung@u-pe.fr>
2012-12-11 17:28:49 +0100

    Cosmétique.

--- Sources/data.f90
+++ Sources/data.f90
@@ -43,7 +43,7 @@ use data_cell
     real(DP), parameter :: rayon1 = .5_DP
     real(DP), parameter :: rmin = 1._DP
     integer, parameter ::  Ncol1 = 270
-    integer, parameter :: Ncolmax = 2 * Ncol1 
+    integer, parameter :: Ncolmax = 2*Ncol1 
     real(DP), dimension(Dim, Ncolmax) :: X
     
 end module data_particles
