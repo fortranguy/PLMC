b5f1e56d9556ec2db75fe6a1081071c765f5b23e
Salomon CHUNG <salomon.chung@u-pe.fr>
2012-12-17 13:13:32 +0100

    Simplification : plot des durées.

--- Scripts/Plots/temps_calc_var.sh
+++ Scripts/Plots/temps_calc_var.sh
@@ -13,27 +13,9 @@ do
 	cd ${outFold_i}${iSimu}
 	
 		vol=$(grep "Vol" rapport.out | cut -d = -f 2)
-              
-        jour_ini=$(awk '$NF=="snapShotIni.out" {print $(NF-2)}' rapport.out)
-        jour_fin=$(awk '$NF=="snapShotFin.out" {print $(NF-2)}' rapport.out)
-        delta_jour=$(expr ${jour_fin} \- ${jour_ini})
-        
-        heure_ini=$(awk '$NF=="snapShotIni.out" {print $(NF-1)}' rapport.out |\
-            cut -d : -f 1)
-        heure_fin=$(awk '$NF=="snapShotFin.out" {print $(NF-1)}' rapport.out |\
-            cut -d : -f 1)
-        delta_heure=$(expr ${heure_fin} \- ${heure_ini})
-            
-        minute_ini=$(awk '$NF=="snapShotIni.out" {print $(NF-1)}' rapport.out |\
-            cut -d : -f 2)
-        minute_fin=$(awk '$NF=="snapShotFin.out" {print $(NF-1)}' rapport.out |\
-            cut -d : -f 2)
-        delta_minute=$(expr ${minute_fin} \- ${minute_ini})
-        
-        total_heure=$(expr ${delta_jour} \* 24 \+ ${delta_heure})
-        total_minute=$(expr ${total_heure} \* 60 \+ ${delta_minute})
+		duree_minute=$(grep "Durée" rapport.out | cut -d = -f 2)
  
-        echo ${vol}"   "${total_minute} >> ../temps_calc.out
+        echo ${vol}"   "${duree_minute} >> ../temps_calc.out
 		
 		echo "Rapport n°"${iSimu}" lu."
 		
