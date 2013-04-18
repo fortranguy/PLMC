31c05652e4657eb735065e8ddd75d77c23ddfd46
Salomon CHUNG <salomon.chung@u-pe.fr>
2013-04-18 17:32:32 +0200

    Fail

--- Sources/class_mixingPotential.f90
+++ Sources/class_mixingPotential.f90
@@ -25,14 +25,135 @@ private
 	
 	contains
 	
-		procedure :: ePot_init => Interacting_ePot_init
-        procedure :: ePot => Interacting_ePot
-        procedure :: ePot_neigh => Interacting_ePot_neigh
-        procedure :: ePot_total => Interacting_ePot_total
-        procedure :: consistTest => Interacting_consistTest
+		procedure :: ePot_init => MixingPotential_ePot_init
+        procedure :: ePot => MixingPotential_ePot
+        procedure :: ePot_neigh => MixingPotential_ePot_neigh
+        procedure :: ePot_total => MixingPotential_ePot_total
 	
 	end type
 	
 contains
+
+!> Potential energy
+    !> Tabulation of Yukawa potential    
+    !> \f[ \epsilon \frac{e^{-\alpha (r-r_{min})}}{r} \f]
+    
+    subroutine MixingPotential_ePot_init(this)
+    
+        class(MixingPotential), intent(inout) :: this
+
+        integer :: i
+        real(DP) :: r_i
+       
+        ! cut
+        do i = this%iMin, this%iCut       
+            r_i = real(i, DP)*this%dr
+            this%ePot_tab(i) = this%epsilon * exp(-this%alpha*(r_i-this%rMin))&
+            /r_i
+        end do
+        
+        ! shift        
+        this%ePot_tab(:) = this%ePot_tab(:) - this%epsilon * &
+            exp(-this%alpha*(this%rCut-this%rMin)) / this%rCut
+
+    end subroutine MixingPotential_ePot_init
+
+    function MixingPotential_ePot(this, r) result(ePot)
+        
+        class(MixingPotential), intent(in) :: this
+        real(DP), intent(in) :: r
+        
+        integer :: i
+        real(DP) :: r_i, ePot
+       
+        if (r < this%rCut) then
+       
+            i = int(r/this%dr)
+            r_i = real(i, DP)*this%dr
+            ePot = this%ePot_tab(i) + (r-r_i)/this%dr * &
+                (this%ePot_tab(i+1)-this%ePot_tab(i))
+           
+        else
+       
+            ePot = 0._DP
+           
+        end if
+        
+    end function MixingPotential_ePot
+    
+    subroutine MixingPotential_ePot_neigh(this, iCol, xCol, iCell, overlap, &
+    	energ)
+        
+        class(MixingPotential), intent(in) :: this        
+        integer, intent(in) :: iCol, iCell
+        real(DP), dimension(Dim), intent(in) :: xCol
+        logical, intent(out) :: overlap
+        real(DP), intent(out) :: energ
+    
+        integer :: iNeigh,  iCell_neigh
+        real(DP) :: r
+    
+        type(Link), pointer :: current => null(), next => null()
+        
+        overlap = .false.
+        energ = 0._DP
+    
+        do iNeigh = 1, cell_neighs_nb
+        
+            iCell_neigh = this%same%cell_neighs(iNeigh, iCell)
+            current => this%same%cellsBegin(iCell_neigh)%particle%next            
+            if (.not. associated(current%next)) cycle
+            
+            do
+            
+                next => current%next
+            
+                if (current%iCol /= iCol) then
+                
+                    r = dist(xCol(:), this%X(:, current%iCol))
+                    if (r < this%rMin) then
+                        overlap = .true.
+                        return
+                    end if
+                    energ = energ + this%ePot(r)
+       
+                end if
+                
+                if (.not. associated(next%next)) exit
+                
+                current => next
+            
+            end do            
+            
+        end do
+    
+    end subroutine MixingPotential_ePot_neigh
+    
+    !> Total potential energy
+    
+    function MixingPotential_ePot_total(this) result(ePot_total)
+    
+        class(MixingPotential), intent(in) :: this
+        
+        integer :: iCol, jCol
+        real(DP) :: r_ij
+        real(DP) :: ePot_total
+    
+        ePot_total = 0._DP
+        
+        do jCol = 1, this%Ncol
+            do iCol = 1, this%Ncol
+                if (iCol /= jCol) then
+                
+                    r_ij = dist(this%X(:, iCol), this%X(:, jCol))
+                    ePot_total = ePot_total + this%ePot(r_ij)
+                    
+                end if
+            end do
+        end do
+        
+        ePot_total = 0.5_DP*ePot_total
+    
+    end function MixingPotential_ePot_total
 
 end module class_mixingPotential
