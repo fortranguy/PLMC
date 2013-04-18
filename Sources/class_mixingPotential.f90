9115d509043591327af62fbe8aeaaa8cc0a45260
Salomon CHUNG <salomon.chung@u-pe.fr>
2013-04-18 16:58:32 +0200

    En cours.

---
+++ Sources/class_mixingPotential.f90
@@ -0,0 +1,38 @@
+!> \brief Description of the Mixing Potential class
+
+module class_mixingPotential
+
+use data_constants
+use data_particles
+use data_potentiel
+
+implicit none
+
+private
+
+	type, public :: MixingPotential
+	
+		private
+		
+		real(DP) :: rMin !< minimum distance between two particles
+		real(DP) :: rCut !< short-range cut
+		real(DP) :: dr !< discretisation step
+        integer :: iMin !< minimum index of tabulation : minimum distance
+        integer :: iCut !< maximum index of tabulation : until potential cut
+        real(DP) :: epsilon !< factor in Yukawa
+        real(DP) :: alpha !< coefficient in Yukawa
+        real(DP), dimension(:), allocatable :: ePot_tab !< tabulation
+	
+	contains
+	
+		procedure :: ePot_init => Interacting_ePot_init
+        procedure :: ePot => Interacting_ePot
+        procedure :: ePot_neigh => Interacting_ePot_neigh
+        procedure :: ePot_total => Interacting_ePot_total
+        procedure :: consistTest => Interacting_consistTest
+	
+	end type
+	
+contains
+
+end module class_mixingPotential
