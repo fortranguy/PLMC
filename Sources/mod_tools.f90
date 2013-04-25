afa482a42e77a5ca7fcb981880452b3319e3fb80
Salomon CHUNG <masterpasteur@gmail.com>
2013-04-26 00:49:57 +0200

    unitReport -> report_unit

--- Sources/mod_tools.f90
+++ Sources/mod_tools.f90
@@ -15,9 +15,9 @@ contains
 
     !> Random number generator : seed
     
-    subroutine init_random_seed(unitReport)
+    subroutine init_random_seed(report_unit)
     
-        integer, intent(in) :: unitReport
+        integer, intent(in) :: report_unit
     
         integer :: i, n, clock
         integer, dimension(:), allocatable :: seed
@@ -30,9 +30,9 @@ contains
         seed(:) = clock + 37 * [ (i - 1, i = 1, n) ]
         call random_seed(put = seed)
         
-        write(unitReport, *) "RNG :"
-        write(unitReport ,*) "    n = ", n
-        write(unitReport ,*) "    seed(:) = ", seed(:)
+        write(report_unit, *) "RNG :"
+        write(report_unit ,*) "    n = ", n
+        write(report_unit ,*) "    seed(:) = ", seed(:)
 
         deallocate(seed)
         
@@ -40,11 +40,11 @@ contains
     
     !> Initial condition
     
-    subroutine initialCondition(type1, type2, mix_rMin, unitReport)
+    subroutine initialCondition(type1, type2, mix_rMin, report_unit)
     
         class(Spheres), intent(inout) :: type1, type2
         real(DP), intent(in) :: mix_rMin
-        integer, intent(in) :: unitReport        
+        integer, intent(in) :: report_unit        
 
         character(len=20) :: init
         integer :: longueur, statut
@@ -53,13 +53,13 @@ contains
         if (statut /= 0) stop "error get_command_argument"
         if (command_argument_count() > 1) stop "Too many arguments"
             
-        write(unitReport, *) "Initial condition :"
+        write(report_unit, *) "Initial condition :"
         
         select case (init)
             case ("rand")
                 call randomDeposition(type1%X, type1%getRMin(), type2%X, &
                     type2%getRmin(), mix_rMin)
-                write(unitReport, *) "    Random deposition"
+                write(report_unit, *) "    Random deposition"
             case default
                 write(output_unit, *) "Enter the initial condition : "
                 write(output_unit, *) "   'rand'."
@@ -126,55 +126,55 @@ contains
     
     !> Report : general
     
-    subroutine report(unitReport)
+    subroutine report(report_unit)
     
-        integer, intent(in) :: unitReport
+        integer, intent(in) :: report_unit
 
-        write(unitReport, *) "Simulation MC_C :"
-        write(unitReport ,*) "    Lsize(:) = ", Lsize(:)
-        write(unitReport ,*) "    Vol = ", product(Lsize)
-        write(unitReport, *) "    Nstep = ", Nstep
-        write(unitReport, *) "    Ntherm = ", Ntherm
-        write(unitReport, *) "    Nmove = ", Nmove
+        write(report_unit, *) "Simulation MC_C :"
+        write(report_unit ,*) "    Lsize(:) = ", Lsize(:)
+        write(report_unit ,*) "    Vol = ", product(Lsize)
+        write(report_unit, *) "    Nstep = ", Nstep
+        write(report_unit, *) "    Ntherm = ", Ntherm
+        write(report_unit, *) "    Nmove = ", Nmove
     
     end subroutine report
     
     !> Results : general
     
-    subroutine results(ePot_mc, ePot_total, ePot_mcSum, duration, unitReport)
+    subroutine results(ePot_mc, ePot_total, ePot_mcSum, duration, report_unit)
     
         real(DP), intent(in) :: ePot_mc, ePot_total, ePot_mcSum
         real(DP), intent(in) :: duration
-        integer, intent(in) :: unitReport
+        integer, intent(in) :: report_unit
         
-        write(unitReport, *) "Consistency test:"
-        write(unitReport, *) "    ePot_mc = ", ePot_mc
-        write(unitReport, *) "    ePot_final = ", ePot_total
-        write(unitReport, *) "    relative difference = ", &
+        write(report_unit, *) "Consistency test:"
+        write(report_unit, *) "    ePot_mc = ", ePot_mc
+        write(report_unit, *) "    ePot_final = ", ePot_total
+        write(report_unit, *) "    relative difference = ", &
             abs((ePot_total-ePot_mc)/ePot_total)
             
-        write(unitReport, *) "Results :"
-        write(unitReport, *) "    average energy = ", &
+        write(report_unit, *) "Results :"
+        write(report_unit, *) "    average energy = ", &
             ePot_mcSum/real(Nstep, DP)
-        write(unitReport, *) "    duration =", duration/60._DP, "min"
+        write(report_unit, *) "    duration =", duration/60._DP, "min"
     
     end subroutine results
     
     !> Results : mix
     
-    subroutine mix_results(ePot_mc, ePot_total, ePot_mcSum, unitReport)
+    subroutine mix_results(ePot_mc, ePot_total, ePot_mcSum, report_unit)
     
         real(DP), intent(in) :: ePot_mc, ePot_total, ePot_mcSum
-        integer, intent(in) :: unitReport
+        integer, intent(in) :: report_unit
 
-        write(unitReport, *) "Consistency test:"
-        write(unitReport, *) "    ePot_mc = ", ePot_mc
-        write(unitReport, *) "    ePot_final = ", ePot_total
-        write(unitReport, *) "    relative difference = ", &
+        write(report_unit, *) "Consistency test:"
+        write(report_unit, *) "    ePot_mc = ", ePot_mc
+        write(report_unit, *) "    ePot_final = ", ePot_total
+        write(report_unit, *) "    relative difference = ", &
             abs((ePot_total-ePot_mc)/ePot_total)
     
-        write(unitReport, *) "Results :"        
-        write(unitReport, *) "    average energy = ", ePot_mcSum/real(Nstep, DP)
+        write(report_unit, *) "Results :"        
+        write(report_unit, *) "    average energy = ", ePot_mcSum/real(Nstep, DP)
     
     end subroutine mix_results
     
