94b28f411953efc1be2bb604bc44f2b45c682523
Salomon CHUNG <salomon.chung@u-pe.fr>
2013-10-24 12:03:01 +0200

    HS_report : simpler

--- Sources/class_dipolarSpheres.f90
+++ Sources/class_dipolarSpheres.f90
@@ -192,21 +192,12 @@ contains
         class(DipolarSpheres), intent(in) :: this
         integer, intent(in) :: report_unit
         
-        write(report_unit, *) "Data :"
-        
-        write(report_unit ,*) "    Ncol = ", this%Ncol
-        write(report_unit ,*) "    Nwidom = ", this%Nwidom
+        call this%HardSpheres%print_report(report_unit)
 
         write(report_unit, *) "    alpha = ", this%alpha
-        write(report_unit, *) "    rCut = ", this%rCut
         write(report_unit, *) "    dr = ", this%dr
         write(report_unit, *) "    reInit_iStep = ", this%reInit_iStep
         write(report_unit, *) "    NwaveVectors = ", this%NwaveVectors
-        
-        write(report_unit, *) "    this_NtotalCell_dim(:) = ", this%sameCells%get_NtotalCell_dim()
-        write(report_unit, *) "    this_cell_size(:) = ", this%sameCells%get_cell_size()
-        write(report_unit, *) "    mix_NtotalCell_dim(:) = ", this%mixCells%get_NtotalCell_dim()
-        write(report_unit, *) "    mix_cell_size(:) = ", this%mixCells%get_cell_size()
         
     end subroutine DipolarSpheres_print_report
     
--- Sources/class_interactingSpheres.f90
+++ Sources/class_interactingSpheres.f90
@@ -118,20 +118,11 @@ contains
         class(InteractingSpheres), intent(in) :: this
         integer, intent(in) :: report_unit
         
-        write(report_unit, *) "Data :"
-        
-        write(report_unit ,*) "    Ncol = ", this%Ncol
-        write(report_unit ,*) "    Nwidom = ", this%Nwidom
+        call this%HardSpheres%print_report(report_unit)
         
         write(report_unit, *) "    epsilon = ", this%epsilon
         write(report_unit, *) "    alpha = ", this%alpha
-        write(report_unit, *) "    rCut = ", this%rCut
         write(report_unit, *) "    dr = ", this%dr
-        
-        write(report_unit, *) "    this_NtotalCell_dim(:) = ", this%sameCells%get_NtotalCell_dim()
-        write(report_unit, *) "    this_cell_size(:) = ", this%sameCells%get_cell_size()
-        write(report_unit, *) "    mix_NtotalCell_dim(:) = ", this%mixCells%get_NtotalCell_dim()
-        write(report_unit, *) "    mix_cell_size(:) = ", this%mixCells%get_cell_size()
         
     end subroutine InteractingSpheres_print_report
     
