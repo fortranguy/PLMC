6781e229906d8dcaf28cdf8c41015267f2358702
Salomon CHUNG <masterpasteur@gmail.com>
2014-02-13 01:12:36 +0100

    more tolerant

--- Sources/data.f90
+++ Sources/data.f90
@@ -13,7 +13,7 @@ implicit none
 
     integer, parameter :: DP = REAL64 !< double precision
     real(DP), parameter :: real_zero = real(2**4, DP) * epsilon(1._DP)
-    real(DP), parameter :: io_tiny = real(2, DP) * epsilon(1._DP)
+    real(DP), parameter :: io_tiny = real(2**2, DP) * epsilon(1._DP)
     real(DP), parameter :: consist_tiny = real(2**13, DP) * epsilon(1._DP)
 
 end module data_precisions
