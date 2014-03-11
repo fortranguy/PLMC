ced52a771568a92dd5bb0bb54a22d1cc0a3a4fe6
Salomon CHUNG <masterpasteur@gmail.com>
2014-03-11 21:43:40 +0100

    hierarchy

--- Sources/module_types.f90
+++ Sources/module_types.f90
@@ -4,6 +4,15 @@ module module_types
 
 implicit none
 
+type, public :: Node
+    integer :: iCol
+    type(Node), pointer :: next => null()
+end type Node
+
+type, public :: LinkedList
+    type(Node), pointer :: particle => null()
+end type LinkedList
+
 type, public :: argument_seed
     character(len=1) :: choice
     integer :: size
@@ -15,14 +24,5 @@ type, public :: argument_initial
     character(len=4096), dimension(3) :: files
     integer, dimension(3) :: length
 end type argument_initial
-
-type, public :: Node
-    integer :: iCol
-    type(Node), pointer :: next => null()
-end type Node
-
-type, public :: LinkedList
-    type(Node), pointer :: particle => null()
-end type LinkedList
 
 end module module_types
