048328ff0c847f7c9a229d081d7855481989e0b2
Salomon CHUNG <salomon.chung@u-pe.fr>
2014-03-13 21:11:22 +0100

    delete usless comments add arguments

--- Sources/class_physicalSystem.f90
+++ Sources/class_physicalSystem.f90
@@ -4,6 +4,7 @@ module class_physicalSystem
 
 use data_precisions, only: DP
 use data_box, only: Ndim
+use module_types, only: argument_seed, argument_initial
 use class_hardSpheres
 use class_dipolarSpheres
 use class_mixingPotential
@@ -18,47 +19,39 @@ private
     
         private
         
-        !> Box
-        real(DP), dimension(Ndim) :: Lsize
-        integer, dimension(Ndim) :: Kmax
+        character(len=5) :: name
         
-        !> Number of particles
-        integer :: Ncol
+        real(DP), dimension(Ndim) :: Lsize !< Box size
+        integer, dimension(Ndim) :: Kmax !< Number of wave vectors
+        integer :: Ncol !< Number of particles
         
-        !> Markov-chain Monte-Carlo
         real(DP) :: Temperature
-        integer :: Nthermal, Nadapt, Nstep
-        
-        !> Changes
-        integer :: Nmove, Nswitch, Nrotate
-        
-        !> Switch
+        integer :: Nthermal, Nadapt, Nstep !< Markov-chain Monte-Carlo
+        integer :: Nmove, Nswitch, Nrotate !< Changes
         integer :: switch_Nhit, switch_Nreject
         real(DP) :: switch_reject, switch_rejectSum
         
-        !> Observables
         real(DP) :: Epot_step, Epot_stepSum, Epot_conf  !< potential energy
         integer :: report_unit !< data & results file
         integer :: obsThermal_unit, obsEquilib_unit !< observables files: thermalisation & equilibrium
         
-        ! Type 1: Dipolar spheres: Ewald summation
         type(DipolarSpheres) :: type1_spheres !< physical properties and Monte-Carlo subroutines
         type(MoreObservables) :: type1_obs !< e.g. energy, inverse of activity (-> chemical potential)
         type(MoreUnits) :: type1_units !< files units
         
-        ! Type 2: Hard spheres
         type(HardSpheres) :: type2_spheres
         type(Observables) :: type2_obs
         type(Units) :: type2_units
         
-        ! Mixing potential between 2 types
         type(MixingPotential) :: mix !< short-range potential
         real(DP) :: mix_Epot_step, mix_Epot_stepSum, mix_Epot_conf
         integer :: mix_report_unit
         integer :: mix_Epot_tab_unit
         integer :: mix_obsThermal_unit, mix_obsEquilib_unit
         
-        !> Time
+        type(argument_seed) :: arg_seed
+        type(argument_initial) :: arg_init
+        
         real(DP) :: time_start, time_end
     
     contains
