31d449addaf7f9d345b309ad3d7873e92e62b020
Salomon CHUNG <salomon.chung@u-pe.fr>
2014-03-13 18:25:48 +0100

    adding physical system class

---
+++ Sources/class_physicalSystem.f90
@@ -0,0 +1,19 @@
+!> \brief Description of the Physical System class
+
+module class_physicalSystem
+
+implicit none
+
+private
+
+    type, public :: PhysicalSystem
+    
+        private
+    
+    contains
+    
+    end type PhysicalSystem
+    
+contains
+
+end module class_physicalSystem
