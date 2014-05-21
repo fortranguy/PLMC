49986a8831fbf3b1467e3f4d47a90d14205a53f1
Salomon CHUNG <salomon.chung@u-pe.fr>
2014-05-21 20:35:00 +0200

    Ewald summation: reci: start

---
+++ Sources/class_ewald_summation_reci.f90
@@ -0,0 +1,22 @@
+module class_ewald_summation_reci
+
+use data_precisions, only: DP
+use data_constants, only: PI
+use data_box, only: Ndim
+use json_module, only: json_file
+use module_data, only: test_data_found
+use class_dipolar_hard_spheres
+
+implicit none
+
+private
+
+    type, public :: Ewald_Summation_Reci
+    
+    contains
+    
+    end type Ewald_Summation_Reci
+    
+contains
+
+end module class_ewald_summation_reci
