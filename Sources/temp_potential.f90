50a5417bd2ae1685ec8088045b16aef24a11e20d
Salomon CHUNG <masterpasteur@gmail.com>
2014-06-12 01:13:00 +0200

    too many details: useless report

--- Sources/temp_potential.f90
+++
@@ -1,5 +0,0 @@
-
-        write(report_unit, *) "    this_NtotalCell_dim(:) = ", this%sameCells%get_NtotalCell_dim()
-        write(report_unit, *) "    this_cell_size(:) = ", this%sameCells%get_cell_size()
-        write(report_unit, *) "    mix_NtotalCell_dim(:) = ", this%mixCells%get_NtotalCell_dim()
-        write(report_unit, *) "    mix_cell_size(:) = ", this%mixCells%get_cell_size()
