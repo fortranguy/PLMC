2b2d9c5644db4ab04d20401b0cab30774d8ffb38
Salomon CHUNG <salomon.chung@u-pe.fr>
2014-07-10 14:03:27 +0200

    snap: default: simple precision

---
+++ Sources/data_write.f90
@@ -0,0 +1,8 @@
+module data_write
+
+implicit none
+
+    character(len=*), parameter :: simple_precision_format = "(3es15.7)"
+    character(len=*), parameter :: double_precision_format = "(3es23.15)"
+
+end module data_write 
