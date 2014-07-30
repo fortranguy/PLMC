84977182497a07d067d294d0e07923eecf0109dd
Salomon CHUNG <salomon.chung@u-pe.fr>
2014-07-30 15:13:03 +0200

    file forgotten

---
+++ Sources/module_geometry.f90
@@ -0,0 +1,11 @@
+module module_geometry
+
+use module_types_micro, only: System_Geometry
+
+implicit none
+private
+public geometry
+
+    type(System_Geometry) :: geometry    
+
+end module module_geometry
