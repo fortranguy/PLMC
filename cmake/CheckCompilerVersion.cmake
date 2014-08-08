787d5f684a03773874a51dfc801df81dfcba8869
Salomon CHUNG <chung.salomon@gmail.com>
2014-08-09 00:45:59 +0200

    added minimum required version of compiler to edit

---
+++ cmake/CheckCompilerVersion.cmake
@@ -0,0 +1,10 @@
+
+if (${CMAKE_Fortran_COMPILER_ID} MATCHES "GNU")
+    if (${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "4.9.1")
+        message(FATAL_ERROR "Insufficient gfortran version")
+    endif ()
+elseif (${CMAKE_Fortran_COMPILER_ID} MATCHES "Intel")
+    if (${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "12.1.0")
+        message(FATAL_ERROR "Insufficient ifort version")
+    endif ()
+endif ()
