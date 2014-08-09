9674a156118442d30bf03e8b3ba1c69b4fab30f8
Salomon CHUNG <masterpasteur@gmail.com>
2014-08-09 23:26:40 +0200

    Print build type.

--- cmake/SelectBuildType.cmake
+++ cmake/SelectBuildType.cmake
@@ -1,12 +1,17 @@
 
 if (NOT CMAKE_BUILD_TYPE)
-    set(build_type_message "Options are: Debug, Release or Fast. Default: Debug.")
-    message(STATUS "Build type: ${build_type_message}")
+    set(
+        build_type_message
+        "Build type options are: Debug, Release or Fast. Default: Debug."
+    )
+    message(STATUS "${build_type_message}")
     set(
         CMAKE_BUILD_TYPE "Debug" CACHE STRING
         ${build_type_message}
         FORCE
     )
+else (NOT CMAKE_BUILD_TYPE)
+    message(STATUS "Build type is: ${CMAKE_BUILD_TYPE}")
 endif (NOT CMAKE_BUILD_TYPE)
 set_property(
     CACHE CMAKE_BUILD_TYPE PROPERTY
