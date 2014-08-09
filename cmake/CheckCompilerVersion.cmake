8170e26f9b50ee1740c309dde198801f374b1605
Salomon CHUNG <masterpasteur@gmail.com>
2014-08-09 03:12:43 +0200

    variable doesn't exist, cf. http://www.cmake.org/pipermail/cmake/2009-April/028726.html

--- cmake/CheckCompilerVersion.cmake
+++ cmake/CheckCompilerVersion.cmake
@@ -1,10 +1,12 @@
 
+message("Compiler version: ${CMAKE_Fortran_COMPILER_VERSION}")
+
 if (${CMAKE_Fortran_COMPILER_ID} MATCHES "GNU")
-    if (${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "4.9.1")
+    if (${CMAKE_Fortran_COMPILER_VERSION} VERSION_LESS "4.9.1")
         message(FATAL_ERROR "Insufficient gfortran version")
     endif ()
 elseif (${CMAKE_Fortran_COMPILER_ID} MATCHES "Intel")
-    if (${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "12.1.0")
+    if (${CMAKE_Fortran_COMPILER_VERSION} VERSION_LESS "12.1.0")
         message(FATAL_ERROR "Insufficient ifort version")
     endif ()
 endif ()
