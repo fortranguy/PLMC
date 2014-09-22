e20e7d2e6b7b61d76f57748170269793a8913b7b
Salomon CHUNG <salomon.chung@u-pe.fr>
2014-09-22 06:13:09 +0200

    adding post processing main

---
+++ src/program_post_processing.f90
@@ -0,0 +1,40 @@
+!> \brief Monte Carlo simulation in canonical ensemble for a mixture
+
+program monte_post_processing
+
+use, intrinsic :: iso_fortran_env, only: output_unit
+use module_types_micro, only: System_Arguments
+use class_system, only: System_Post_Processing
+use module_arguments_monte_carlo, only: read_arguments_post
+
+implicit none
+    
+    type(System_Post_Processing) :: sys
+    type(System_Arguments) :: args
+    
+    integer :: i_step
+
+    call read_arguments_post(args)
+    call sys%construct(args)
+    call sys%init(args)
+        
+    write(output_unit, *) "Beginning of cycles"
+    
+    call sys%set_time_start()
+    MC_Cycle: do i_step = sys%get_num_thermalisation_steps() + 1, &
+                          sys%get_num_thermalisation_steps() + sys%get_num_equilibrium_steps()
+
+            call sys%measure_chemical_potentials()
+            call sys%accumulate_observables()
+            !call sys%write_observables(i_step)
+            call sys%reset_quantites(i_step)
+    
+    end do MC_Cycle
+    call sys%set_time_end()
+
+    write(output_unit, *) "End of cycles"
+    
+    call sys%final()
+    call sys%destroy()
+    
+end program monte_post_processing
