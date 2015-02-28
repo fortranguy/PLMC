4f6db6345832e677140c31b15236b14392a4d92d
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-02-28 16:12:04 +0100

    Box class: size and wave getters to implement

--- lib/src/class_box_geometry.f90
+++ lib/src/class_box_geometry.f90
@@ -7,4 +7,15 @@ implicit none
 
 private
 
+    type, abstract, public :: Abstract_Box_Geometry
+    private
+        real(DP), dimension(num_dimensions) :: size
+        integer, dimension(num_dimensions) :: wave
+    contains
+        procedure :: get_size => Abstract_Box_Geometry_get_size
+        procedure :: get_wave => Abstract_Box_Geometry_get_wave
+    end type Abstract_Box_Geometry
+    
+contains
+
 end module class_box_geometry
