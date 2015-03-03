a1aa943445172cffc3e62097ad5b9dc61dfabdd5
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-03-03 16:59:04 +0100

    Random_Moments: ctor() and dtor() added

--- lib/src/class_random_moments.f90
+++ lib/src/class_random_moments.f90
@@ -12,11 +12,25 @@ private
     type, abstract, public :: Abstract_Random_Moments
     private
     contains
+        procedure(Abstract_Random_Moments_construct), deferred :: construct
+        procedure(Abstract_Random_Moments_destroy), deferred :: destroy
         procedure(Abstract_Random_Moments_moment), deferred :: moment
         procedure(Abstract_Random_Moments_rotation), deferred :: rotation
     end type Abstract_Random_Moments
 
     abstract interface
+    
+        subroutine Abstract_Random_Moments_construct(this, dipolar_spheres, delta)
+        import :: DP, Abstract_Dipolar_Spheres, Abstract_Random_Moments
+            class(Abstract_Random_Moments), intent(out) :: this
+            class(Abstract_Dipolar_Spheres), target, intent(in) :: dipolar_spheres
+            real(DP), intent(in) :: delta
+        end subroutine Abstract_Random_Moments_construct
+        
+        subroutine Abstract_Random_Moments_destroy(this)
+        import :: Abstract_Random_Moments
+            class(Abstract_Random_Moments), intent(inout) :: this
+        end subroutine Abstract_Random_Moments_destroy
     
         function Abstract_Random_Moments_moment(this) result(moment)
         import :: DP, num_dimensions, Abstract_Random_Moments
@@ -35,6 +49,8 @@ private
 
     type, extends(Abstract_Random_Moments) :: Null_Random_Moments
     contains
+        procedure :: construct => Null_Random_Moments_construct
+        procedure :: destroy => Null_Random_Moments_destroy
         procedure :: moment => Null_Random_Moments_moment
         procedure :: rotation => Null_Random_Moments_rotation
     end type Null_Random_Moments
@@ -43,6 +59,8 @@ private
         real(DP) :: normed_delta
         class(Abstract_Dipolar_Spheres), pointer :: dipolar_spheres => null()
     contains
+        procedure :: construct => Random_Moments_construct
+        procedure :: destroy => Random_Moments_destroy
         procedure :: moment => Random_Moments_moment
         procedure :: rotation => Random_Moments_rotation
     end type Random_Moments
@@ -50,6 +68,18 @@ private
 contains
 
 !implementation Null_Random_Moments
+
+    subroutine Null_Random_Moments_construct(this, dipolar_spheres, delta)
+        class(Null_Random_Moments), intent(out) :: this
+        class(Abstract_Dipolar_Spheres), target, intent(in) :: dipolar_spheres
+        real(DP), intent(in) :: delta
+        
+    end subroutine Null_Random_Moments_construct
+    
+    subroutine Null_Random_Moments_destroy(this)
+        class(Null_Random_Moments), intent(inout) :: this
+        
+    end subroutine Null_Random_Moments_destroy
 
     function Null_Random_Moments_moment(this) result(moment)
         class(Null_Random_Moments), intent(in) :: this
@@ -70,7 +100,21 @@ contains
 
 !implementation Random_Moments
 
-    !> From SMAC, Algorithm 1.23, p. 43
+    subroutine Random_Moments_construct(this, dipolar_spheres, delta)
+        class(Random_Moments), intent(out) :: this
+        class(Abstract_Dipolar_Spheres), target, intent(in) :: dipolar_spheres
+        real(DP), intent(in) :: delta
+        
+        this%dipolar_spheres => dipolar_spheres
+        this%normed_delta = delta / this%dipolar_spheres%get_moment_norm()
+    end subroutine Random_Moments_construct
+    
+    subroutine Random_Moments_destroy(this)
+        class(Random_Moments), intent(inout) :: this
+        
+        this%dipolar_spheres => null()
+    end subroutine Random_Moments_destroy
+    
     function Random_Moments_moment(this) result(moment)
         class(Random_Moments), intent(in) :: this
         real(DP) :: moment(num_dimensions)
