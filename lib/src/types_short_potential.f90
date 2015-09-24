872660624032496ffad7503abd1a3f9c4fda2429
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-09-24 21:23:17 +0200

    misplaced files added

---
+++ lib/src/types_short_potential.f90
@@ -0,0 +1,21 @@
+module types_short_potential
+
+use class_potential_expression, only: Abstract_Potential_Expression
+use class_pair_potential, only: Abstract_Pair_Potential
+use class_particles_potential, only: Abstract_Particles_Potential
+use class_visitable_list, only: Abstract_Visitable_List
+use class_visitable_cells, only: Abstract_Visitable_Cells
+
+implicit none
+
+private
+
+    type, public :: Short_Potential_Wrapper
+        class(Abstract_Potential_Expression), allocatable :: potential_expression
+        class(Abstract_Pair_Potential), allocatable :: pair_potential
+        class(Abstract_Particles_Potential), allocatable :: particle_potential
+        class(Abstract_Visitable_List), allocatable :: visitable_list
+        class(Abstract_Visitable_Cells), allocatable :: visitable_cells
+    end type Short_Potential_Wrapper
+
+end module types_short_potential
