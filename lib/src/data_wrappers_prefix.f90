0a408a07031fbd0fdd368a30444f318e84f03c57
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-10-06 03:48:36 +0200

    forgotten file

---
+++ lib/src/data_wrappers_prefix.f90
@@ -0,0 +1,15 @@
+module data_wrappers_prefix
+
+implicit none
+
+private
+public :: environment_prefix, mixtures_prefix, changes_prefix, short_potentials_prefix, &
+    ewalds_prefix
+
+    character(len=*), parameter :: environment_prefix = "Environment."
+    character(len=*), parameter :: mixtures_prefix = "Mixture."
+    character(len=*), parameter :: changes_prefix = "Changes."
+    character(len=*), parameter :: short_potentials_prefix = "Short Potentials."
+    character(len=*), parameter :: ewalds_prefix = "Ewalds."
+
+end module data_wrappers_prefix
