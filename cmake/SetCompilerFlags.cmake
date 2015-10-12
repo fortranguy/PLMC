ffb0480fa4404b699bc8c9c2de294bb13839dc4d
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-10-12 08:31:36 +0200

    useless flags

--- cmake/SetCompilerFlags.cmake
+++ cmake/SetCompilerFlags.cmake
@@ -9,16 +9,7 @@ elseif (${CMAKE_Fortran_COMPILER_ID} MATCHES "Intel")
     set(CMAKE_Fortran_FLAGS_FAST "-ipo -O3 -static-intel -no-prec-div -xHost")
 endif (${CMAKE_Fortran_COMPILER_ID} MATCHES "GNU")
 
-if (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
-    force_add_flags(CMAKE_CXX_FLAGS_DEBUG "-W -Wall") 
-    set(CMAKE_CXX_FLAGS_FAST "-Ofast")
-elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
-    force_add_flags(CMAKE_CXX_FLAGS_DEBUG "-W -Wall")
-    set(CMAKE_CXX_FLAGS_FAST "-O3 -static-intel -no-prec-div -xHost")
-endif (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
-
 mark_as_advanced(
     CMAKE_Fortran_FLAGS_FAST
-    CMAKE_CXX_FLAGS_FAST
 )
 
