878968e0fcf6da1c42156682d3ea2ee0471054ef
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-10-29 07:19:02 +0100

    gfortran extra flags added

--- cmake/SetCompilerFlags.cmake
+++ cmake/SetCompilerFlags.cmake
@@ -1,6 +1,6 @@
 
 if (${CMAKE_Fortran_COMPILER_ID} MATCHES "GNU")
-    force_add_flags(CMAKE_Fortran_FLAGS_DEBUG "-W -Wall -fbounds-check")
+    force_add_flags(CMAKE_Fortran_FLAGS_DEBUG "-W -Wall -Wextra -fbounds-check -g")
     set(CMAKE_Fortran_FLAGS_FAST "-Ofast")
 elseif (${CMAKE_Fortran_COMPILER_ID} MATCHES "Intel")
     force_add_flags(CMAKE_Fortran_FLAGS_DEBUG
