236d8f4d3827432ecf4e2a73cddc32f28516c33a
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-10-29 20:41:16 +0100

    Main program source: moved to programs/
    Documentation md & C::B: updated after reorganisation

--- modules/mixture/procedures_component_factory.f90
+++ modules/mixture/procedures_component_factory.f90
@@ -17,8 +17,7 @@ use class_component_dipolar_moments, only: Abstract_Component_Dipolar_Moments, &
 use class_component_total_moment, only: Abstract_Component_Total_Moment, &
     Concrete_Component_Total_Moment, Null_Component_Total_Moment
 use types_component_wrapper, only: Component_Wrapper
-use procedures_property_inquirers, only: use_walls, component_is_dipolar, &
-    component_can_exchange
+use procedures_property_inquirers, only: use_walls, component_is_dipolar, component_can_exchange
 
 implicit none
 
--- plmc.md
+++ plmc.md
@@ -1,8 +1,8 @@
 project: Physics of Liquids Monte-Carlo
 summary: A simple Monte-Carlo program for colloids
     written in Modern Fortran
-project_dir: ./src
-    ./lib/src
+project_dir: ./programs
+    ./modules
 extra_mods: json_module: http://jacobwilliams.github.io/json-fortran/
 output_dir: ./doc
 predocmark: >
@@ -14,3 +14,4 @@ year: 2015
 author: Salomon CHUNG
 email: salomon.chung@u-pe.fr
 website: http://www.plmc.u-pec.fr/
+
