d1134d20b5ef786ddff609ce8c7061d5cffe3540
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-11-20 04:07:19 +0100

    EwaldReciStructure: coordinates updates
    Coordinates delta doc updated
    Coordiantes change structure factor: implemented

--- modules/long_interactions/class_ewald_reci_structure.f90
+++ modules/long_interactions/class_ewald_reci_structure.f90
@@ -1,5 +1,3 @@
-!! display: public
-!!          private
 module class_ewald_reci_structure
 
 use, intrinsic :: iso_fortran_env, only: DP => REAL64
@@ -30,6 +28,8 @@ private
         procedure :: reset => Abstract_Ewald_Reci_Structure_set
         procedure :: get => Abstract_Ewald_Reci_Structure_get
         procedure, private :: set => Abstract_Ewald_Reci_Structure_set
+        procedure :: get_coordinates_delta => Abstract_Ewald_Reci_Structure_get_coordinates_delta
+        procedure :: set_coordinates_delta => Abstract_Ewald_Reci_Structure_set_coordinates_delta
     end type Abstract_Ewald_Reci_Structure
 
 contains
@@ -118,14 +118,14 @@ contains
         structure = this%structure(n_1, n_2, n_3)
     end function Abstract_Ewald_Reci_Structure_get
 
-    !> Coordinate change: energy delta when component \( I \) changes.
+    !> Energy delta when a particle \( i \) of component \( I \) changes its coordinates.
     !> \[
     !>      \Delta U_{I, J} = \sum_{\vec{k}} w_\alpha(k)
-    !>          \Re[(s^{\prime{}I}_l(\vec{k}) - s^I_l(\vec{k})) S_J(\vec{k})^\ast] +
-    !>          [I=J] \frac{1}{2}|s^{\prime{}I}_l(\vec{k}) - s^I_l(\vec{k})|^2
+    !>          \Re[(s^{\prime{}I}_i(\vec{k}) - s^I_i(\vec{k})) S_J(\vec{k})^\ast] +
+    !>          [I=J] \frac{1}{2}|s^{\prime{}I}_i(\vec{k}) - s^I_i(\vec{k})|^2
     !> \]
     !> with \( s_i(\vec{k}) = (\vec{k}\cdot\vec{\mu}_i) e^{i\vec{k}\cdot\vec{x}_i}\).
-    pure real(DP) function Abstract_Ewald_Reci_Structure_coordinates_delta(this, new, old, &
+    pure real(DP) function Abstract_Ewald_Reci_Structure_get_coordinates_delta(this, new, old, &
         same_component) result(delta_energy)
         class(Abstract_Ewald_Reci_Structure), intent(in) :: this
         type(Concrete_Temporary_Particle), intent(in) :: new, old
@@ -188,7 +188,67 @@ contains
                 end do
             end do
         end do
-    end function Abstract_Ewald_Reci_Structure_coordinates_delta
+        delta_energy = 2._DP * delta_energy ! half wave vector (symmetry) -> double energy
+    end function Abstract_Ewald_Reci_Structure_get_coordinates_delta
+
+    !> Structure factor update when a particle \( i \) changes its coordinates.
+    !>  \[ \Delta S(\vec{k}) = s_i^\prime(\vec{k}) - s_i(\vec{k}) \]
+    pure subroutine Abstract_Ewald_Reci_Structure_set_coordinates_delta(this, new, old)
+        class(Abstract_Ewald_Reci_Structure), intent(inout) :: this
+        type(Concrete_Temporary_Particle), intent(in) :: new, old
+
+        real(DP) :: box_size(num_dimensions)
+        real(DP), dimension(num_dimensions) :: wave_vector
+        integer :: n_1, n_2, n_3
+        complex(DP) :: structure_wave_new, structure_wave_old
+        real(DP), dimension(num_dimensions) :: wave_1_x_position_new, wave_1_x_position_old
+        real(DP) :: wave_dot_moment_new, wave_dot_moment_old
+
+        complex(DP) :: fourier_position_new
+        complex(DP), dimension(-this%reci_numbers(1):this%reci_numbers(1)) :: fourier_position_new_1
+        complex(DP), dimension(-this%reci_numbers(2):this%reci_numbers(2)) :: fourier_position_new_2
+        complex(DP), dimension(-this%reci_numbers(3):this%reci_numbers(3)) :: fourier_position_new_3
+
+        complex(DP) :: fourier_position_old
+        complex(DP), dimension(-this%reci_numbers(1):this%reci_numbers(1)) :: fourier_position_old_1
+        complex(DP), dimension(-this%reci_numbers(2):this%reci_numbers(2)) :: fourier_position_old_2
+        complex(DP), dimension(-this%reci_numbers(3):this%reci_numbers(3)) :: fourier_position_old_3
+
+        box_size = this%periodic_box%get_size()
+
+        wave_1_x_position_old = 2._DP*PI * old%position / box_size
+        call set_fourier(fourier_position_old_1, this%reci_numbers(1), wave_1_x_position_old(1))
+        call set_fourier(fourier_position_old_2, this%reci_numbers(2), wave_1_x_position_old(2))
+        call set_fourier(fourier_position_old_3, this%reci_numbers(3), wave_1_x_position_old(3))
+
+        wave_1_x_position_new = 2._DP*PI * new%position / box_size
+        call set_fourier(fourier_position_new_1, this%reci_numbers(1), wave_1_x_position_new(1))
+        call set_fourier(fourier_position_new_2, this%reci_numbers(2), wave_1_x_position_new(2))
+        call set_fourier(fourier_position_new_3, this%reci_numbers(3), wave_1_x_position_new(3))
+
+        ! Warning: only half wave vectors are updated
+        do n_3 = 0, this%reci_numbers(3)
+            wave_vector(3) = 2._DP*PI * real(n_3, DP) / box_size(3)
+            do n_2 = -reci_number_2_sym(this%reci_numbers, n_3), this%reci_numbers(2)
+                wave_vector(2) = 2._DP*PI * real(n_2, DP) / box_size(2)
+                do n_1 = -reci_number_1_sym(this%reci_numbers, n_3, n_2), this%reci_numbers(1)
+                    wave_vector(1) = 2._DP*PI * real(n_1, DP) / box_size(1)
+
+                    fourier_position_old = fourier_position_old_1(n_1) * &
+                        fourier_position_old_2(n_2) * fourier_position_old_3(n_3)
+                    fourier_position_new = fourier_position_new_1(n_1) * &
+                        fourier_position_new_2(n_2) * fourier_position_new_3(n_3)
+                    wave_dot_moment_old = dot_product(wave_vector, old%dipolar_moment)
+                    wave_dot_moment_new = dot_product(wave_vector, new%dipolar_moment)
+
+                    structure_wave_new = wave_dot_moment_new * fourier_position_new
+                    structure_wave_old = wave_dot_moment_old * fourier_position_old
+                    this%structure(n_1, n_2, n_3) = this%structure(n_1, n_2, n_3) + &
+                        structure_wave_new - structure_wave_old
+                end do
+            end do
+        end do
+    end subroutine Abstract_Ewald_Reci_Structure_set_coordinates_delta
 
 !end implementation Abstract_Ewald_Reci_Structure
 
