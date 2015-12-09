c4e3a606fda39cd0f7acc134dcb2f4720d4f71f0
Salomon CHUNG <salomon.chung@u-pe.fr>
2015-12-09 06:37:19 +0100

    EwaldReci: optimisation: move
    The goal is to optimise Ewald Summation energy delta
    calculation by writing specific procedures for
    move and rotation.
    - visit_move_delta() and set_move_delta() implemented.

--- modules/long_interactions/class_ewald_reci_component.f90
+++ modules/long_interactions/class_ewald_reci_component.f90
@@ -31,6 +31,8 @@ private
         procedure :: visit_coordinates_delta => &
             Abstract_Ewald_Reci_Component_visit_coordinates_delta
         procedure :: set_coordinates_delta => Abstract_Ewald_Reci_Component_set_coordinates_delta
+        procedure :: visit_move_delta => Abstract_Ewald_Reci_Component_visit_move_delta
+        procedure :: set_move_delta => Abstract_Ewald_Reci_Component_set_move_delta
     end type Abstract_Ewald_Reci_Component
 
     type, extends(Abstract_Ewald_Reci_Component), public :: Concrete_Ewald_Reci_Component
@@ -208,6 +210,83 @@ contains
         delta_energy = 2._DP * delta_energy ! half wave vector (symmetry) -> double energy
     end function Abstract_Ewald_Reci_Component_visit_coordinates_delta
 
+    !> Energy delta when a particle \( l \) of component \( I \) moves.
+    !> \[
+    !>      \Delta U_{I, J} = \sum_{\vec{k}} w_\alpha(k) (\vec{k}\cdot\vec{\mu}_l) \{
+    !>          \Re[(e^{i\vec{k}\cdot\vec{x}^\prime_l} - e^{i\vec{k}\cdot\vec{x}_l})
+    !>              S_J(\vec{k})^\ast] +
+    !>          [I=J] (\vec{k}\cdot\vec{\mu}_l)
+    !>              [1 - \Re(e^{i\vec{k}\cdot\vec{x}^\prime_l}e^{-i\vec{k}\cdot\vec{x}_l})]
+    !>          \}
+    !> \]
+    !> with \( s_i(\vec{k}) = (\vec{k}\cdot\vec{\mu}_i) e^{i\vec{k}\cdot\vec{x}_i} \).
+    pure real(DP) function Abstract_Ewald_Reci_Component_visit_move_delta(this, new, old, &
+        same_component) result(delta_energy)
+        class(Abstract_Ewald_Reci_Component), intent(in) :: this
+        type(Concrete_Temporary_Particle), intent(in) :: new, old
+        logical, intent(in) :: same_component
+
+        real(DP) :: box_size(num_dimensions)
+        real(DP), dimension(num_dimensions) :: wave_vector
+        integer :: n_1, n_2, n_3
+        real(DP), dimension(num_dimensions) :: wave_1_x_position_new, wave_1_x_position_old
+        real(DP) :: real_delta_fourier_x_conjg_structure, wave_dot_moment
+
+        complex(DP) :: fourier_position_new
+        complex(DP), dimension(-this%reci_numbers(1):this%reci_numbers(1)) :: fourier_position_new_1
+        complex(DP), dimension(-this%reci_numbers(2):this%reci_numbers(2)) :: fourier_position_new_2
+        complex(DP), dimension(-this%reci_numbers(3):this%reci_numbers(3)) :: fourier_position_new_3
+
+        complex(DP) :: fourier_position_old
+        complex(DP), dimension(-this%reci_numbers(1):this%reci_numbers(1)) :: fourier_position_old_1
+        complex(DP), dimension(-this%reci_numbers(2):this%reci_numbers(2)) :: fourier_position_old_2
+        complex(DP), dimension(-this%reci_numbers(3):this%reci_numbers(3)) :: fourier_position_old_3
+
+        box_size = this%periodic_box%get_size()
+
+        wave_1_x_position_old = 2._DP*PI * old%position / box_size
+        call set_fourier(fourier_position_old_1, this%reci_numbers(1), wave_1_x_position_old(1))
+        call set_fourier(fourier_position_old_2, this%reci_numbers(2), wave_1_x_position_old(2))
+        call set_fourier(fourier_position_old_3, this%reci_numbers(3), wave_1_x_position_old(3))
+
+        wave_1_x_position_new = 2._DP*PI * new%position / box_size
+        call set_fourier(fourier_position_new_1, this%reci_numbers(1), wave_1_x_position_new(1))
+        call set_fourier(fourier_position_new_2, this%reci_numbers(2), wave_1_x_position_new(2))
+        call set_fourier(fourier_position_new_3, this%reci_numbers(3), wave_1_x_position_new(3))
+
+        delta_energy = 0._DP
+        do n_3 = 0, this%reci_numbers(3)
+            wave_vector(3) = 2._DP*PI * real(n_3, DP) / box_size(3)
+            do n_2 = -reci_number_2_sym(this%reci_numbers, n_3), this%reci_numbers(2)
+                wave_vector(2) = 2._DP*PI * real(n_2, DP) / box_size(2)
+                do n_1 = -reci_number_1_sym(this%reci_numbers, n_3, n_2), this%reci_numbers(1)
+                    wave_vector(1) = 2._DP*PI * real(n_1, DP) / box_size(1)
+
+                    fourier_position_old = fourier_position_old_1(n_1) * &
+                        fourier_position_old_2(n_2) * fourier_position_old_3(n_3)
+                    fourier_position_new = fourier_position_new_1(n_1) * &
+                        fourier_position_new_2(n_2) * fourier_position_new_3(n_3)
+                    wave_dot_moment = dot_product(wave_vector, old%dipolar_moment)
+
+                    real_delta_fourier_x_conjg_structure = &
+                        real((fourier_position_new - fourier_position_old) * &
+                        conjg(this%structure(n_1, n_2, n_3)), DP)
+
+                    if (same_component) then
+                        delta_energy = delta_energy + this%weight%get(n_1, n_2, n_3) * &
+                            wave_dot_moment * (real_delta_fourier_x_conjg_structure + &
+                            wave_dot_moment * (1._DP - real(fourier_position_new * &
+                                conjg(fourier_position_old), DP)))
+                    else
+                        delta_energy = delta_energy + this%weight%get(n_1, n_2, n_3) * &
+                            wave_dot_moment * real_delta_fourier_x_conjg_structure
+                    end if
+                end do
+            end do
+        end do
+        delta_energy = 2._DP * delta_energy ! half wave vector (symmetry) -> double energy
+    end function Abstract_Ewald_Reci_Component_visit_move_delta
+
     !> Structure factor update when a particle \( i \) changes its coordinates.
     !>  \[ \Delta S(\vec{k}) = s_i^\prime(\vec{k}) - s_i(\vec{k}) \]
     pure subroutine Abstract_Ewald_Reci_Component_set_coordinates_delta(this, new, old)
@@ -264,6 +343,62 @@ contains
             end do
         end do
     end subroutine Abstract_Ewald_Reci_Component_set_coordinates_delta
+
+    !> Structure factor update when a particle \( l \) moves.
+    !>  \[ \Delta S(\vec{k}) = (\vec{k}\cdot\vec{\mu}_l)
+    !>                         (e^{i\vec{k}\cdot\vec{x}^\prime_l} - e^{i\vec{k}\cdot\vec{x}_l}) \]
+    pure subroutine Abstract_Ewald_Reci_Component_set_move_delta(this, new, old)
+        class(Abstract_Ewald_Reci_Component), intent(inout) :: this
+        type(Concrete_Temporary_Particle), intent(in) :: new, old
+
+        real(DP) :: box_size(num_dimensions)
+        real(DP), dimension(num_dimensions) :: wave_vector
+        integer :: n_1, n_2, n_3
+        real(DP), dimension(num_dimensions) :: wave_1_x_position_new, wave_1_x_position_old
+        real(DP) :: wave_dot_moment
+
+        complex(DP) :: fourier_position_new
+        complex(DP), dimension(-this%reci_numbers(1):this%reci_numbers(1)) :: fourier_position_new_1
+        complex(DP), dimension(-this%reci_numbers(2):this%reci_numbers(2)) :: fourier_position_new_2
+        complex(DP), dimension(-this%reci_numbers(3):this%reci_numbers(3)) :: fourier_position_new_3
+
+        complex(DP) :: fourier_position_old
+        complex(DP), dimension(-this%reci_numbers(1):this%reci_numbers(1)) :: fourier_position_old_1
+        complex(DP), dimension(-this%reci_numbers(2):this%reci_numbers(2)) :: fourier_position_old_2
+        complex(DP), dimension(-this%reci_numbers(3):this%reci_numbers(3)) :: fourier_position_old_3
+
+        box_size = this%periodic_box%get_size()
+
+        wave_1_x_position_old = 2._DP*PI * old%position / box_size
+        call set_fourier(fourier_position_old_1, this%reci_numbers(1), wave_1_x_position_old(1))
+        call set_fourier(fourier_position_old_2, this%reci_numbers(2), wave_1_x_position_old(2))
+        call set_fourier(fourier_position_old_3, this%reci_numbers(3), wave_1_x_position_old(3))
+
+        wave_1_x_position_new = 2._DP*PI * new%position / box_size
+        call set_fourier(fourier_position_new_1, this%reci_numbers(1), wave_1_x_position_new(1))
+        call set_fourier(fourier_position_new_2, this%reci_numbers(2), wave_1_x_position_new(2))
+        call set_fourier(fourier_position_new_3, this%reci_numbers(3), wave_1_x_position_new(3))
+
+        ! Warning: only half wave vectors are updated
+        do n_3 = 0, this%reci_numbers(3)
+            wave_vector(3) = 2._DP*PI * real(n_3, DP) / box_size(3)
+            do n_2 = -reci_number_2_sym(this%reci_numbers, n_3), this%reci_numbers(2)
+                wave_vector(2) = 2._DP*PI * real(n_2, DP) / box_size(2)
+                do n_1 = -reci_number_1_sym(this%reci_numbers, n_3, n_2), this%reci_numbers(1)
+                    wave_vector(1) = 2._DP*PI * real(n_1, DP) / box_size(1)
+
+                    fourier_position_old = fourier_position_old_1(n_1) * &
+                        fourier_position_old_2(n_2) * fourier_position_old_3(n_3)
+                    fourier_position_new = fourier_position_new_1(n_1) * &
+                        fourier_position_new_2(n_2) * fourier_position_new_3(n_3)
+
+                    this%structure(n_1, n_2, n_3) = this%structure(n_1, n_2, n_3) + &
+                        dot_product(wave_vector, old%dipolar_moment) * &
+                        (fourier_position_new - fourier_position_old)
+                end do
+            end do
+        end do
+    end subroutine Abstract_Ewald_Reci_Component_set_move_delta
 
 !end implementation Abstract_Ewald_Reci_Component
 
