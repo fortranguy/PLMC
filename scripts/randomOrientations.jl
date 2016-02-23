1c45dda33bc88e5a12821a269603ae9152f424e1
Salomon CHUNG <salomon.chung@u-pe.fr>
2016-02-23 16:54:48 +0100

    RandomConfigurations: modules in uppercase

--- scripts/randomOrientations.jl
+++ scripts/randomOrientations.jl
@@ -9,26 +9,24 @@ module RandomOrientations
 end
 
 import JSON
-json = JSON
 import RandomOrientations
-ro = RandomOrientations
+RO = RandomOrientations
 include("PLMC.jl")
 import PLMC
-plmc = PLMC
 
 if size(ARGS, 1) == 0
     error("Please provide a .json file.")
 end
-input_data = json.parsefile(ARGS[1]; dicttype=Dict, use_mmap=true)
+input_data = JSON.parsefile(ARGS[1]; dicttype=Dict, use_mmap=true)
 num_components = input_data["Mixture"]["number of components"]
 if num_components == 0
     exit(0)
 end
 for i_component = 1:num_components
     if (input_data["Mixture"]["Component $(i_component)"]["is dipolar"])
-        component_i = plmc.Component(input_data["Mixture"]["Component $(i_component)"]["number"],
+        component_i = PLMC.Component(input_data["Mixture"]["Component $(i_component)"]["number"],
                                      zeros(3, 1), zeros(3, 1))
-        component_i.orientations = ro.randomOrientations(component_i.num)
+        component_i.orientations = RO.randomOrientations(component_i.num)
         output_file = input_data["Mixture"]["Component $(i_component)"]["initial orientations"]
         writedlm(output_file, component_i.orientations)
         println("Orientations written in ", output_file)
