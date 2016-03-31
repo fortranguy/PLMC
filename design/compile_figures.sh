7154f01899009431049ddb44ee66e21b64512489
Salomon CHUNG <chung.salomon@gmail.com>
2016-03-31 21:17:01 +0200

    scale added
    - multithread compiler script added
    - DipolarInterfactions must be split

---
+++ design/compile_figures.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+
+if (($# != 1))
+then
+    echo "Usage: compile_figures [NUM THREADS]"
+    exit 1
+fi
+
+trap "wait" TERM EXIT
+
+for tex in $(find figures/ -name "*.tex")
+do
+    echo $tex
+    (time pdflatex -output-directory=$(dirname $tex) $tex) > \
+        $(dirname $tex)/$(basename $tex .tex).tmp 2>&1&
+    while (($(jobs | wc -l) >= $@))
+    do
+        sleep 0.1
+        jobs > /dev/null
+    done
+done
