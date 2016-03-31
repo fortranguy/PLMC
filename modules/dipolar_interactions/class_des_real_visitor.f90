48855daad2a7e529cbd936e853eee519015c1fbe
Salomon CHUNG <salomon.chung@u-pe.fr>
2016-03-31 04:55:40 +0200

    Ewald: start documenting in UML
    - grammar correction

--- modules/dipolar_interactions/class_des_real_visitor.f90
+++ modules/dipolar_interactions/class_des_real_visitor.f90
@@ -75,23 +75,23 @@ contains
         end do
     end subroutine Abstract_visit_inter
 
-    pure subroutine Abstract_visit_intra(this, energy, positions, dipolar_moment, des_real_pair)
+    pure subroutine Abstract_visit_intra(this, energy, positions, dipolar_moments, des_real_pair)
         class(Abstract_DES_Real_Visitor), intent(in) :: this
         real(DP), intent(out) :: energy
         class(Abstract_Component_Coordinates), intent(in) :: positions
-        class(Abstract_Component_Dipolar_Moments), intent(in) :: dipolar_moment
+        class(Abstract_Component_Dipolar_Moments), intent(in) :: dipolar_moments
         class(Abstract_DES_Real_Pair), intent(in) :: des_real_pair
 
         real(DP) :: vector_ij(num_dimensions)
         integer :: i_particle, j_particle
 
         energy = 0._DP
-        do j_particle = 1, dipolar_moment%get_num()
-            do i_particle = j_particle + 1, dipolar_moment%get_num()
+        do j_particle = 1, dipolar_moments%get_num()
+            do i_particle = j_particle + 1, dipolar_moments%get_num()
                 vector_ij = this%periodic_box%vector(positions%get(i_particle), positions%&
                     get(j_particle))
-                energy = energy + des_real_pair%meet(vector_ij, dipolar_moment%get(i_particle), &
-                    dipolar_moment%get(j_particle))
+                energy = energy + des_real_pair%meet(vector_ij, dipolar_moments%get(i_particle), &
+                    dipolar_moments%get(j_particle))
             end do
         end do
     end subroutine Abstract_visit_intra
@@ -120,11 +120,11 @@ contains
         energy = 0._DP
     end subroutine Null_visit_inter
 
-    pure subroutine Null_visit_intra(this, energy, positions, dipolar_moment, des_real_pair)
+    pure subroutine Null_visit_intra(this, energy, positions, dipolar_moments, des_real_pair)
         class(Null_DES_Real_Visitor), intent(in) :: this
         real(DP), intent(out) :: energy
         class(Abstract_Component_Coordinates), intent(in) :: positions
-        class(Abstract_Component_Dipolar_Moments), intent(in) :: dipolar_moment
+        class(Abstract_Component_Dipolar_Moments), intent(in) :: dipolar_moments
         class(Abstract_DES_Real_Pair), intent(in) :: des_real_pair
         energy = 0._DP
     end subroutine Null_visit_intra
