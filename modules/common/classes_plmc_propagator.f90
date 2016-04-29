2c04e1041e73dd0b84b89bd9ce97e1bbb6b84c2d
Salomon CHUNG <salomon.chung@u-pe.fr>
2016-04-29 04:30:30 +0200

    PLMCpropagator: set_selector() & try() added

--- modules/common/classes_plmc_propagator.f90
+++ modules/common/classes_plmc_propagator.f90
@@ -3,6 +3,7 @@ module classes_plmc_propagator
 use classes_tower_sampler, only: Abstract_Tower_Sampler
 use types_metropolis_algorithms_wrapper, only: Metropolis_Algorithm_Pointer, &
     Metropolis_Algorithms_Wrapper
+use types_observables_wrapper, only: Observables_Wrapper
 
 implicit none
 
@@ -15,6 +16,7 @@ private
     contains
         procedure :: construct => Abstract_construct
         procedure :: destroy => Abstract_destroy
+        procedure :: set_selector => Abstract_set_selector
     end type Abstract_PLMC_Propagator
 
 contains
@@ -35,5 +37,23 @@ contains
         end if
         this%metropolis_algorithms => null()
     end subroutine Abstract_destroy
+
+    subroutine Abstract_set_selector(this, selector)
+        class(Abstract_PLMC_Propagator), intent(inout) :: this
+        class(Abstract_Tower_Sampler), intent(in) :: selector
+
+        allocate(this%selector, source=selector)
+    end subroutine Abstract_set_selector
+
+    subroutine Abstract_try(this, observables)
+        class(Abstract_PLMC_Propagator), intent(in) :: this
+        type(Observables_Wrapper), intent(inout) :: observables
+
+        integer :: i_choice
+
+        do i_choice = 1, this%selector%get_num_choices()
+            call this%metropolis_algorithms(this%selector%get())%algorithm%try(observables)
+        end do
+    end subroutine Abstract_try
 
 end module classes_plmc_propagator
