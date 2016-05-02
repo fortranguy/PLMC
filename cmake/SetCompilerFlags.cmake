7c769234015451dfdb738aba1e95b9d79e89d3f6
Salomon CHUNG <salomon.chung@u-pe.fr>
2016-05-03 01:37:30 +0200

    CMake: WIP: IPO

--- cmake/SetCompilerFlags.cmake
+++ cmake/SetCompilerFlags.cmake
@@ -7,10 +7,9 @@ elseif (${CMAKE_Fortran_COMPILER_ID} MATCHES "Intel")
     force_add_flags(CMAKE_Fortran_FLAGS_DEBUG
         "-warn all -check all -check noarg_temp_created -fltconsistency -traceback"
     )
-    set(CMAKE_Fortran_FLAGS_FAST "-ipo -O3 -static-intel -no-prec-div -xHost")
+    set(CMAKE_Fortran_FLAGS_FAST "-fast")
 endif (${CMAKE_Fortran_COMPILER_ID} MATCHES "GNU")
 
 mark_as_advanced(
     CMAKE_Fortran_FLAGS_FAST
 )
-
