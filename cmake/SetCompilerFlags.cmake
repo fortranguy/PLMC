23fa7b3926a0ab5005cb97db5b14e8ade44ff254
Salomon CHUNG <salomon.chung@u-pe.fr>
2016-05-03 06:12:52 +0200

    gfortran: Pointers optimisation added

--- cmake/SetCompilerFlags.cmake
+++ cmake/SetCompilerFlags.cmake
@@ -2,7 +2,7 @@
 if (${CMAKE_Fortran_COMPILER_ID} MATCHES "GNU")
     force_add_flags(CMAKE_Fortran_FLAGS_DEBUG "-W -Wall -Wextra -fbounds-check -g -fbacktrace\
         -ffpe-trap=zero,overflow,underflow")
-    set(CMAKE_Fortran_FLAGS_FAST "-Ofast")
+    set(CMAKE_Fortran_FLAGS_FAST "-Ofast -fipa-pta")
 elseif (${CMAKE_Fortran_COMPILER_ID} MATCHES "Intel")
     force_add_flags(CMAKE_Fortran_FLAGS_DEBUG
         "-warn all -check all -check noarg_temp_created -fltconsistency -traceback"
